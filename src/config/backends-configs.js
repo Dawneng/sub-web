1f9ae096935fee35fd246d33651c239c73fd9c3b
DawnDeng <socialdng365@gmail.com>
2026-03-07 21:46:46 +0800

    update

--- src/config/backends-configs.js
+++ src/config/backends-configs.js
@@ -1,6 +1,6 @@
 // 预设后端配置选项
 export const BACKENDS_CONFIGS = [
-  { label: 'api.auiwe.com(默认后端)', value: import.meta.env.VITE_SUBCONVERTER_DEFAULT_BACKEND + '/sub?'},
-  { label: 'api.asailor.org(SubConverter-Extended 作者提供)', value: 'https://api.asailor.org/sub?' },
+  { label: 'sub.auiwe.com(默认后端)', value: 'https://sub.auiwe.com/sub?' },
+  { label: 'api.auiwe.com(传统后端)', value: import.meta.env.VITE_SUBCONVERTER_DEFAULT_BACKEND + '/sub?'},
   { label: '127.0.0.1:25500(本地后端)', value: 'http://127.0.0.1:25500/sub?' }
 ];
