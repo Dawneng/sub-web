3d6da7cdf83831ef1ff316721de176ba1ac66d2c
DawnDeng <socialdng365@gmail.com>
2026-05-11 20:27:35 +0800

    fix: 修改后端地址

--- src/config/backends-configs.js
+++ src/config/backends-configs.js
@@ -1,6 +1,6 @@
 // 预设后端配置选项
 export const BACKENDS_CONFIGS = [
-  { label: 'sub.auiwe.com(默认后端)', value: 'https://sub.auiwe.com/sub?' },
-  { label: 'api.auiwe.com(传统后端)', value: import.meta.env.VITE_SUBCONVERTER_DEFAULT_BACKEND + '/sub?'},
+  { label: 'api.aiuwe.me(默认后端)', value: 'https://api.aiuwe.me/sub?' },
+  { label: 'sub.aiuwe.me(传统后端)', value: import.meta.env.VITE_SUBCONVERTER_DEFAULT_BACKEND + '/sub?'},
   { label: '127.0.0.1:25500(本地后端)', value: 'http://127.0.0.1:25500/sub?' }
 ];
