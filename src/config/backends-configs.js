25b87168e016ef65745e06d664e2e8d44e9d8278
DawnDeng <socialdng365@gmail.com>
2026-05-17 19:32:25 +0800

    Merge master branche

--- src/config/backends-configs.js
+++ src/config/backends-configs.js
@@ -1,6 +1,6 @@
 // 预设后端配置选项
 export const BACKENDS_CONFIGS = [
-  { label: 'api.uivwe.com(默认后端)', value: import.meta.env.VITE_SUBCONVERTER_DEFAULT_BACKEND + '/sub?'},
-  { label: 'api.asailor.org(SubConverter-Extended)', value: 'https://api.asailor.org/sub?' },
+  { label: 'api.aiuwe.me(默认后端)', value: 'https://api.aiuwe.me/sub?' },
+  { label: 'sub.aiuwe.me(传统后端)', value: import.meta.env.VITE_SUBCONVERTER_DEFAULT_BACKEND + '/sub?'},
   { label: '127.0.0.1:25500(本地后端)', value: 'http://127.0.0.1:25500/sub?' }
 ];
